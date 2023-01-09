079ce2dec5b7344e845b0e8b185f316bd636646f
Dustin Vo <datvo.vtd@gmail.com>
2023-01-08 22:41:27 -0600

    ci

--- script/build.sh
+++
@@ -1,9 +0,0 @@
-echo "========> Build UI Content..."
-yarn install && yarn bootstrap
-yarn build --scope=frontend
-
-echo "========> Copying UI Content To Server..."
-cp -r src/frontend/packages/app/dist/ src/backend/client/
-
-echo "=========> Building Server"
-yarn build --scope=backend
