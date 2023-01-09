b95bc7108d3beae9b4ba4179399f1fce6e70ead9
Dustin Vo <datvo.vtd@gmail.com>
2023-01-08 22:58:05 -0600

    ci

--- build-fullstack.sh
+++ build-fullstack.sh
@@ -1,3 +1,6 @@
+echo "[FULLSTACK-APP] - Installing Dependencies..."
 yarn install && yarn bootstrap
+
+echo "[FULLSTACK-APP] - Build App"
 yarn build
 cp -r src/frontend/packages/app/dist/ src/backend/client/
